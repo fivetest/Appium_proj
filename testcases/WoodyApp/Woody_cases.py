98c624b2cd85e994d0ff79f65beb4f88abe98508
viprs <viprs@qq.com>
2017-12-02 17:22:19 +0800

    测试用例取消注释

--- testcases/WoodyApp/Woody_cases.py
+++ testcases/WoodyApp/Woody_cases.py
@@ -4,21 +4,21 @@ from lib.AndroidDriver import AndroidDriver
 
 
 class WoodyApps(unittest.TestCase):
-    # def test_swipe_down(self):
-    #     """打开啄木鸟的app， 向下滑动，然后检查日期的按钮是否能看见"""
-    #     ad = AndroidDriver()
-    #     ad.swipe_to_down()
-    #     # 需要确认按钮能否找到
-    #     self.assertTrue(ad.is_element_appearance('cc.liushi.testapp:id/date_button'))
-    #     # 截图
-    #     ad.capture_screenshots()
-    #
-    #
-    # def test_popup_window(self):
-    #     """"点击按钮，指定文本会显示出来"""
-    #     ad = AndroidDriver()
-    #     ad.click_by_id('showPopupWindowButtonCD')
-    #     ad.capture_screenshots()
+    def test_swipe_down(self):
+        """打开啄木鸟的app， 向下滑动，然后检查日期的按钮是否能看见"""
+        ad = AndroidDriver()
+        ad.swipe_to_down()
+        # 需要确认按钮能否找到
+        self.assertTrue(ad.is_element_appearance('cc.liushi.testapp:id/date_button'))
+        # 截图
+        ad.capture_screenshots()
+
+
+    def test_popup_window(self):
+        """"点击按钮，指定文本会显示出来"""
+        ad = AndroidDriver()
+        ad.click_by_id('showPopupWindowButtonCD')
+        ad.capture_screenshots()
 
     def test_text_is_visible(self):
         """点击一个按钮以后，文本会显出"""
