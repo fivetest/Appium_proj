13855afada57888190a114dada88ea4267265853
viprs <viprs@qq.com>
2017-12-02 17:21:51 +0800

    项目目录的优化

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
-from testcases.Woody_cases import WoodyApps
+import time
 import unittest
+
+from lib.HTMLTestRunner import HTMLTestRunner
+from testcases.WoodyApp.Woody_cases import WoodyApps
 
 
 def get_suite():
@@ -12,6 +15,11 @@ def get_suite():
 
 if __name__ == "__main__":
     suite = get_suite()
-    runner = unittest.TextTestRunner()
+    fp = open('./reports/results_%s.html' % time.strftime("%Y-%m-%d %H-%M-%S"), 'wb')
+    runner = HTMLTestRunner(
+        stream=fp,
+        title=u'啄木鸟移动APP测试报告',
+        description=u"测试用例执行情况：")
     runner.run(suite)
+    fp.close()
 
