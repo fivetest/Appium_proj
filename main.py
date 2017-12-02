2c0c75a2f8dc6872849f7d06a5118f8fb0b96268
viprs <viprs@qq.com>
2017-12-02 11:33:23 +0800

    添加main函数，并执行测试用例

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python
+from testcases.Woody_cases import WoodyApps
+import unittest
 
+
+def get_suite():
+    suite = unittest.TestSuite()
+    loader = unittest.TestLoader()
+    suite.addTests(loader.loadTestsFromTestCase(WoodyApps))
+    return suite
+
+
+if __name__ == "__main__":
+    suite = get_suite()
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
 
